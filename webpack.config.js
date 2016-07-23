2b2bb4522702ff1c94704f638c06dd6e5e9eb196
Hodaka Suzuki <altitude3190@gmail.com>
2016-07-23 23:08:32 +0900

    introduction of webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+var config = {
+  // module: {
+  //   loaders: [
+  //     {
+  //       test: /\.js?$/,
+  //       exclude: /node_modules/,
+  //       loader: 'babel?cacheDirectory'
+  //     },
+  //     {
+  //       test: /\.styl?$/,
+  //       exclude: /(node_modules|bower_components)/,
+  //       loader: 'style-loader!css-loader!stylus-loader'
+  //     }
+  //   ]
+  // },
+  entry: './lib/app.js',
+  output: {
+    path: path.join(__dirname, 'compiled'),
+    filename: 'app.js',
+    // sourceMapFilename: '[name].map',
+    // libraryTarget: 'commonjs2',
+    // publicPath: 'http://localhost:8080/assets/'
+  },
+  // debug: true,
+  // devtool: 'eval-source-map'
+};
+
+module.exports = config;
