c26153e7cd5962c455d8830e9cb75db75da123c0
Hodaka Suzuki <altitude3190@gmail.com>
2017-05-14 14:28:00 +0900

    modify for eslint

--- src/js/components/Note.js
+++ src/js/components/Note.js
@@ -18,61 +18,64 @@ export default {
         this.vm.saveAtRegularInterval({ noteModel });
 
         // display nothing
-        if (!noteModel) return <div id='note-container' class='column'></div>
+        if (!noteModel) return <div id="note-container" className="column"></div>;
 
         // preview mode view
         if (this.vm.isPreviewMode()) {
-            return <div id='note-container' class='column'>
-              <h1 id="note-title"> { noteModel.title() } </h1>
-              <div class="tabs is-boxed">
-                <ul>
-                  <li class="is-active">
-                    <a>
-                      <span class="icon is-small"><i class="fa fa-play"></i></span>
-                      <span>Render</span>
-                    </a>
-                  </li>
-                  <li onclick={ this.vm.switchMode.bind(this.vm) }>
-                    <a>
-                      <span class="icon is-small"><i class="fa fa-edit"></i></span>
-                      <span>Edit</span>
-                    </a>
-                  </li>
-                </ul>
+            return (
+              <div id="note-container" className="column">
+                <h1 id="note-title"> {noteModel.title()} </h1>
+                <div className="tabs is-boxed">
+                  <ul>
+                    <li className="is-active">
+                      <a>
+                        <span className="icon is-small"><i className="fa fa-play"></i></span>
+                        <span>Render</span>
+                      </a>
+                    </li>
+                    <li onclick={this.vm.switchMode.bind(this.vm)}>
+                      <a>
+                        <span className="icon is-small"><i className="fa fa-edit"></i></span>
+                        <span>Edit</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="markdown-body">
+                  {m.trust(marked(noteModel.content()))}
+                </div>
               </div>
-              <div class="markdown-body">
-                { m.trust(marked(noteModel.content())) }
-              </div>
-            </div>
+            );
         }
 
         // edit mode view
-        return <div id='note-container' class='column'>
-          <form>
-            <input type="text" id="note-title" value={ noteModel.title() } onblur={ m.withAttr('value', this.vm.updateNoteTitle.bind(this.vm)) }></input>
-          </form>
-          <div class="tabs is-boxed">
-            <ul>
-              <li onclick={ this.vm.switchMode.bind(this.vm) }>
-                <a>
-                  <span class="icon is-small"><i class="fa fa-play"></i></span>
-                  <span>Render</span>
-                </a>
-              </li>
-              <li class="is-active">
-                <a>
-                  <span class="icon is-small"><i class="fa fa-edit"></i></span>
-                  <span>Edit</span>
-                </a>
-              </li>
-            </ul>
+        return (
+          <div id="note-container" className="column">
+            <form>
+              <input
+                type="text" id="note-title" value={noteModel.title()}
+                onblur={m.withAttr('value', this.vm.updateNoteTitle, this.vm)}
+              ></input>
+            </form>
+            <div className="tabs is-boxed">
+              <ul>
+                <li onclick={this.vm.switchMode.bind(this.vm)}>
+                  <a>
+                    <span className="icon is-small"><i className="fa fa-play"></i></span>
+                    <span>Render</span>
+                  </a>
+                </li>
+                <li className="is-active">
+                  <a>
+                    <span className="icon is-small"><i className="fa fa-edit"></i></span>
+                    <span>Edit</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <textarea id="note-textarea">{noteModel.content()}</textarea>
           </div>
-          <textarea id="note-textarea">{noteModel.content()}</textarea>
-        </div>
+        );
     },
 
 };
-
-
-
-
--- src/js/components/NoteList.js
+++ src/js/components/NoteList.js
@@ -11,22 +11,37 @@ export default {
     },
 
     view() {
-        return <div id= 'note-list-container' class='column is-3'>
-          <div id='note-list-header'>
-            <button id="note-create-btn" onclick={ this.vm.createNewNote }>
-              <span class="icon is-medium"><i class="fa fa-plus-circle" aria-hidden="true"></i></span>
-            </button>
+        return (
+          <div id="note-list-container" className="column is-3">
+            <div id="note-list-header">
+              <button id="note-create-btn" onclick={this.vm.createNewNote}>
+                <span className="icon is-medium">
+                  <i className="fa fa-plus-circle" aria-hidden="true"></i>
+                </span>
+              </button>
+            </div>
+            <ul id="note-list">
+              {
+                this.vm.getDisplayNoteModels().map((model) => {
+                    return (
+                      <li
+                        className="note-list-note"
+                        note-id={model.id()}
+                        onclick={m.withAttr('note-id', this.vm.onClickNote)}
+                        oncontextmenu={
+                          m.withAttr('note-id', this.vm.showContextMenu, this.vm)
+                        }
+                      >
+                        <p className="note-list-note-title">{model.title()}</p>
+                        <p className="note-list-note-updated-at">
+                          updated: {moment.unix(model.updatedAt()).format('YYYY/MM/DD HH:mm')}
+                        </p>
+                      </li>
+                    );
+                })
+              }
+            </ul>
           </div>
-          <ul id='note-list'>
-            {
-              this.vm.getDisplayNoteModels().map((model) => {
-                return <li class="note-list-note" note-id={ model.id() } onclick={ m.withAttr('note-id', this.vm.onClickNote) } oncontextmenu={ m.withAttr('note-id', this.vm.showContextMenu, this.vm) }>
-                  <p class="note-list-note-title">{ model.title() }</p>
-                  <p class="note-list-note-updated-at">updated: { moment.unix(model.updatedAt()).format('YYYY/MM/DD HH:mm') }</p>
-                </li>
-              })
-            }
-          </ul>
-        </div>
+        );
     },
 };
